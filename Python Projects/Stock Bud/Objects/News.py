37c6c0d1f4541163c349d10578e1f13658cec753
“Daniel <nwogod@myumanitoba.ca>
2025-04-17 15:02:44 -0500

    Testing commits

--- Python Projects/Stock Bud/Objects/News.py
+++ Python Projects/Stock Bud/Objects/News.py
@@ -9,15 +9,5 @@ class News:
         return f"Title:{self.title}\nDescription: {self.description}\nDate: {self.date}\nSource: {self.source}"
     
     
-class News2:
-    def __init__(self, title, description, date, source):
-        self.title = title
-        self.description = description
-        self.date = date
-        self.source = source
-        
-    def __str__(self):
-        return f"Title:{self.title}\nDescription: {self.description}\nDate: {self.date}\nSource: {self.source}"
-
 
 
