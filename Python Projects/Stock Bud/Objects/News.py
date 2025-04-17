12a1ed1c10da8107c15823eb03e8459bb94271ab
“Daniel <nwogod@myumanitoba.ca>
2025-04-17 15:10:27 -0500

    Fixed stuff

--- Python Projects/Stock Bud/Objects/News.py
+++ Python Projects/Stock Bud/Objects/News.py
@@ -7,22 +7,7 @@ class News:
         
         
         
-    
-    class News1:
-        def __init__(self, title, description, date, source):
-            self.title = title
-            self.description = description
-            self.date = date
-            self.source = source
         
-        
-        
-    class News2:
-        def __init__(self, title, description, date, source):
-            self.title = title
-            self.description = description
-            self.date = date
-            self.source = source
         
     def __str__(self):
         return f"Title:{self.title}\nDescription: {self.description}\nDate: {self.date}\nSource: {self.source}"
