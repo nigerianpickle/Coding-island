f90c141e45b668d14b0e622ecc2c432435d8141a
“Daniel <nwogodaniel123@gmail.com>
2025-04-19 00:55:05 -0500

    Added a portolio website framework. Tried to get the NewsExtractor working, didn't work :(.

--- Python Projects/Stock Bud/Cli/NewsExtractor.py
+++ Python Projects/Stock Bud/Cli/NewsExtractor.py
@@ -1,9 +1,9 @@
 #Author: Daniel Nwogo
 #Date: 2023-10-04
 #imports
-import News
+from News import News
 import newspaper
-from newspaper import Article
+from newspaper import Article,Config
 
 DEFAULT_NEWS_URL="https://ca.finance.yahoo.com/news/nvidias-2025-has-been-anything-but-easy-and-its-going-to-get-tougher-171847622.html"
 
@@ -19,8 +19,9 @@ class NewsExtractor:
 
 
     def extractTitleFromUrl(self, url):
-        extractedTitle = url[31:-4]
-        print(extractedTitle)
+        extractedTitle = url[32:-15]
+        extractedTitle = extractedTitle.replace("-", " ")
+
         
     #Returns a news article objects
     def extractNews(self,url):
@@ -28,22 +29,64 @@ class NewsExtractor:
         article=Article(url)
         article.download()
         article.parse()
-        article.nlp()
+        # article.nlp()
         
 
         
         #News object
-        news=News(
-            title=article.title,
-            description=article.summary,
-            date=article.publish_date,
-            source=article.source_url,
-            data=article.text.strip()
-        )
+        if article.title == None:
+            news=News(
+                title=self.extractTitleFromUrl(url),
+                description=article.summary,
+                date=article.publish_date,
+                source=article.source_url,
+                data=article.text.strip()
+            )
+        elif article.publish_date == None:
+            news=News(
+                title=article.title,
+                description=article.summary,
+                date="Unknown",
+                source=article.source_url,
+                data=article.text.strip()
+            )
+        elif article.source_url == None:
+            news=News(
+                title=article.title,
+                description=article.summary,
+                date=article.publish_date,
+                source="Unknown",
+                data=article.text.strip()
+            )   
+        elif article.text == None:
+            news=News(
+                title=article.title,
+                description=article.summary,
+                date=article.publish_date,
+                source=article.source_url,
+                data="Unknown"
+            )
+        elif article.text == "":
+            news=News(
+                title=article.title,
+                description=article.summary,
+                date=article.publish_date,
+                source=article.source_url,
+                data="Unknown"
+            )
+        else:
+            news=News(
+                title=article.title,
+                description=article.summary,
+                date=article.publish_date,
+                source=article.source_url,
+                data=article.text.strip()
+            )
         
         self.lastNews = news
         self.prevNews[news.title] = news
         return news
+    
 
     #
     #Used to extract multiple news articles from a list of urls
@@ -52,29 +95,49 @@ class NewsExtractor:
         for url in urls:
             news = self.extractNews(url)
             newsList.append(news)
+            self.prevNews[news.title] = news
         return newsList
     
     #Used to extract multiple urls from a News homepage
     def extractMultipleUrls(self, url):
         urlList = []
         
-        news_paper=newspaper.build(url)
+
+        news_paper=newspaper.build(url, memoize_articles=False)
         
-        print("Just checking the urls")
         for article in news_paper.articles:
             #skip
-            # if article.url in urlList:
-            #     continue
+            if article.url in urlList:
+                continue
             
-            
-            if article.title == None:
-                print("No title found")
-                self.extractTitleFromUrl(article.url)
-                
-            
-            print(article.url)
-            print(article.title)
+
             urlList.append(article.url)
+        
+        return urlList
+    
+    def extractNewsFromMainPage(self, url):
+        urlList = self.extractMultipleUrls(url)
+        
+        newsList= self.extractMultipleNews(urlList)
+        
+        #List of each news article
+        return newsList
+        
+    def printNews(self):
+        for news in self.prevNews:
+            print("Title:", news.title)
+            print("Description:", news.description)
+            print("Date:", news.date)
+            print("Source:", news.source)
+            print("Data:", news.data)
+            print("")
+            
+            
+#Test the extractor
+testExtractor = NewsExtractor()
+testExtractor.extractNews("https://www.cnbc.com/")
+
+            
             
             
 
@@ -83,4 +146,4 @@ class NewsExtractor:
         
 
 testExtractor = NewsExtractor()
-testExtractor.extractMultipleUrls("https://www.cnn.com/markets")
+testExtractor.extractNewsFromMainPage("https://ca.finance.yahoo.com/")
