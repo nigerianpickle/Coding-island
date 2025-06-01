6ff8bb3ecea3957a0fc9c14480c6fb34e5393830
“Daniel <nwogodaniel123@gmail.com>
2025-05-31 22:59:00 -0500

    Pushed some bullshit

--- Python Projects/Python Neural Network/NeuralNetwork.py
+++ Python Projects/Python Neural Network/NeuralNetwork.py
@@ -1,5 +1,19 @@
+import numpy as np
+from Neuron import Neuron
+
 INITIAL_WEIGHT=np.array([0.5, 0.5])  # Example initial weights
 
 class NeuralNetwork:
     def __init__(self):
+        self.weights= INITIAL_WEIGHT
+        self.bias = 0.5
+        
+        
+        #Hidden layer
+        self.h1==Neuron(sel.weights,self.bias)
+        self.h2 = Neuron(self.weights, self.bias)
+        
+        
+        #Output layer
+        self.o1 = Neuron(self.weights, self.bias)
         
